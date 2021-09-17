b2b7b9d548de8bf7234447d3a7b703db76e5103d
gu_delta <y1030432@edu.gifu-u.ac.jp>
2021-09-17 13:08:26 +0900

    Fix #8: Add the function to edit a post in MySQL Database.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,7 +6,9 @@
 		{{-- &laquo; <a href="/">Back</a> --}}
 		&laquo; <a href="{{ route('posts.index') }}">Back</a>
 	</div>
-
-	<h1>{{ $post->title }}</h1>
+	<h1>
+		<span>{{ $post->title }}</span>
+		<a href="{{ route('posts.edit', $post) }}">[Edit]</a>
+	</h1>
 	<p>{!! nl2br(e($post->body)) !!}
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,7 @@ Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show')
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 
 Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit')->where('post', '[0-9]+');
+
+Route::patch('/posts/{post}/update', [PostController::class, 'update'])->name('posts.update')->where('post', '[0-9]+');
